596297fb87cebd7d1231413bcd067739f2c372a0
vooit <wojciechpac@gmail.com>
2017-07-24 22:30:19 +0200

    ss

---
+++ src/scripts/DemoComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ListComponent from './ListFilter';
+
+class DemoComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <ListComponent />
+            </div>
+        )
+    }
+}
+
+export default DemoComponent;
---
+++ src/scripts/main.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import DemoComponent from './DemoComponent';
+
+import '../styles/styles.scss';
+import 'bootstrap-sass/assets/stylesheets/_bootstrap.scss';
+
+ReactDOM.render(<DemoComponent/>, document.getElementById('app'));
